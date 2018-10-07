c77229898988c5ceb75d1b49f8701e9086443d08
Ben Robertson <35968223+bwrobertson@users.noreply.github.com>
2018-10-06 18:49:16 -0600

    Update fileServer.py
    Actually fixed port to work with stammer proxy

--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -2,7 +2,7 @@ import socket, os               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 50000                 # Reserve a port for your service.
+port = 50001                 # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 s.listen(5)                 # Now wait for client connection.
 picture = ""
